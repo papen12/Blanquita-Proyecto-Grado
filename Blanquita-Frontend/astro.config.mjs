64b1484f8e0e8029777966185b959244b346b36e
papen12 <papenfabri2.0@gmail.com>
2026-07-22 20:00:06 -0400

    Agregar ShadCn

--- Blanquita-Frontend/astro.config.mjs
+++ Blanquita-Frontend/astro.config.mjs
@@ -1,11 +1,25 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import node from '@astrojs/node';
+import path from 'node:path';
+
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   output: 'server',
+
   adapter: node({
     mode: 'standalone'
   }),
-  integrations: [react()]
+
+  integrations: [react()],
+
+  vite: {
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve('./src'),
+      },
+    },
+  },
 });
